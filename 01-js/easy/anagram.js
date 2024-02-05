7d2b3763747ba9f323add25e64d14d0c239060fd
Ahad-23 <ahad.shaikkh@gmail.com>
2024-02-05 22:41:08 +0530

    .

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,19 @@
 */
 
 function isAnagram(str1, str2) {
+  str1=str1.toUpperCase()
+  str2=str2.toUpperCase()
+  // Check if the lengths of the two strings are the same
+  if (str1.length !== str2.length) {
+    return false;
+  }
 
+ 
+  const sortedStr1 = str1.split('').sort().join('');
+  const sortedStr2 = str2.split('').sort().join('');
+
+  
+  return sortedStr1 === sortedStr2;
 }
 
 module.exports = isAnagram;
