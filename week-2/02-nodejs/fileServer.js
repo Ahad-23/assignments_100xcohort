9cf29b7411bb19a4e008173b31574040e738ac7b
Ahad-23 <ahad.shaikkh@gmail.com>
2024-03-06 00:10:35 +0530

    Update fileServer.js

--- week-2/02-nodejs/fileServer.js
+++ week-2/02-nodejs/fileServer.js
@@ -12,10 +12,44 @@
     - For any other route not defined in the server return 404
     Testing the server - run `npm run test-fileServer` command in terminal
  */
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const app = express();
-
-
-module.exports = app;
+    const express = require('express');
+    const fs = require('fs');
+    const path = require('path');
+    const app = express();
+    
+    const filesDirectory = path.join(__dirname, 'files');
+    
+    app.get('/files', function(req, res) {
+      fs.readdir(filesDirectory, function(err, files) {
+        if (err) {
+          res.status(500).send('Internal Server Error');
+          return;
+        }
+        res.status(200).json(files);
+      });
+    });
+    
+    app.get('/file/:filename', function(req, res) {
+      const filename = req.params.filename;
+      const filePath = path.join(filesDirectory, filename);
+    
+      fs.readFile(filePath, 'utf-8', function(err, data) {
+        if (err) {
+          res.status(404).send('File not found');
+          return;
+        }
+        res.status(200).send(data);
+      });
+    });
+    
+    // For any other route not defined in the server, return 404
+    app.use(function(req, res) {
+      res.status(404).send('Not Found');
+    });
+    
+    app.listen(3000, function() {
+      console.log('Server is running on port 3000');
+    });
+    
+    module.exports = app;
+    
